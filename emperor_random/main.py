e0f00681e69762f72089aa41501a7ac010d577d2
HeartyYF <dongwench@126.com>
2024-08-30 23:53:04 +0800

    update random bot

--- emperor_random/main.py
+++ emperor_random/main.py
@@ -30,10 +30,13 @@ class MessageList:
                 self.data.append(MessageInstance(item['type'], item['message']))
     
     def add_message(self, messageI):
-        self.data.append(messageI)
-        with open('data.json', 'w') as f:
-            data = [x.__dict__ for x in self.data]
-            json.dump(data, f)
+        if messageI not in self.data:
+            self.data.append(messageI)
+            with open('data.json', 'w') as f:
+                data = [x.__dict__ for x in self.data]
+                json.dump(data, f)
+            return True
+        return False
     
     def find_messages(self, messageI):
         for i in self.data:
@@ -47,6 +50,15 @@ class MessageList:
             if text in i.message:
                 result.append(i)
         return result
+    
+    def remove_message(self, messageI):
+        if messageI in self.data:
+            self.data.remove(messageI)
+            with open('data.json', 'w') as f:
+                data = [x.__dict__ for x in self.data]
+                json.dump(data, f)
+            return True
+        return False
 
     def get_random(self, num=1):
         result = random.sample(self.data, num)
@@ -56,6 +68,9 @@ class MessageList:
 
 msg_list = MessageList()
 emperor_id = 'YOUR_EMPEROR'
+
+with open('disable.json', 'r') as f:
+    disable_list = json.load(f)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="随机皇帝语录！")
@@ -68,9 +83,7 @@ async def send_message(bot, chat_id, messageI):
 
 async def on_private_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     msg = update.message
-    # await context.bot.send_message(chat_id=update.effective_chat.id, text=msg.forward_origin)
     if msg.forward_origin and isinstance(msg.forward_origin, MessageOriginUser):
-        # await context.bot.send_message(chat_id=update.effective_chat.id, text=msg.forward_origin.sender_user.id)
         if msg.forward_origin.sender_user.id == emperor_id:
             await context.bot.send_message(chat_id=update.effective_chat.id, text="是皇帝！")
             text = msg.text
@@ -88,10 +101,13 @@ async def on_private_message(update: Update, context: ContextTypes.DEFAULT_TYPE)
     await send_message(context.bot, update.effective_chat.id, random_chat)
 
 async def on_group_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_id = update.effective_chat.id
+    if chat_id in disable_list:
+        return
     random_number = random.randint(0, 99)
     if random_number < 5:
         random_chat = msg_list.get_random()
-        await send_message(context.bot, update.effective_chat.id, random_chat)
+        await send_message(context.bot, chat_id, random_chat)
 
 async def on_channel_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     msg = update.channel_post
@@ -107,6 +123,32 @@ async def on_channel_message(update: Update, context: ContextTypes.DEFAULT_TYPE)
 async def random_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     random_chat = msg_list.get_random()
     await send_message(context.bot, update.effective_chat.id, random_chat)
+
+async def remove_certain_msg(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    msg = update.message
+    text = msg.text
+    command = '/remove '
+    command_with_name = '/remove@lhpqbot '
+    if text.startswith(command_with_name):
+        text = text[len(command_with_name):]
+    elif text.startswith(command):
+        text = text[len(command):]
+    messageI = MessageInstance('text', text)
+    if msg_list.remove_message(messageI):
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="已删除该语录！")
+    else:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="该语录不存在！")
+
+async def toggle_enable(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_id = update.effective_chat.id
+    if chat_id in disable_list:
+        disable_list.remove(chat_id)
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="已启用！")
+    else:
+        disable_list.append(chat_id)
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="已禁用！")
+    with open('disable.json', 'w') as f:
+        json.dump(disable_list, f)
 
 async def inline_query(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Handle the inline query. This is run when you type: @botusername <query>"""    
@@ -140,13 +182,20 @@ async def inline_query(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
 
     await update.inline_query.answer(results)
 
+async def reload_data(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global msg_list
+    msg_list = MessageList()
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="数据已重新加载！")
+
 if __name__ == '__main__':
     application = ApplicationBuilder().token('TOKEN').build()
     
     start_handler = CommandHandler('start', start)
     random_handler = CommandHandler('random', random_message)
+    reload_handler = CommandHandler('reload', reload_data)
+    remove_handler = CommandHandler('remove', remove_certain_msg)
+    toggle_handler = CommandHandler('toggle', toggle_enable, filters.ChatType.GROUPS)
     
-    # message_handler = MessageHandler(filters.TextFilter('/empty'), del_inline_message)
     private_msg_handler = MessageHandler((~filters.COMMAND) & filters.ChatType.PRIVATE, on_private_message)
     group_msg_handler = MessageHandler((~filters.COMMAND) & filters.ChatType.GROUPS, on_group_message)
     channel_msg_handler = MessageHandler((~filters.COMMAND) & filters.ChatType.CHANNEL, on_channel_message)
@@ -155,6 +204,9 @@ if __name__ == '__main__':
     
     application.add_handler(start_handler)
     application.add_handler(random_handler)
+    application.add_handler(reload_handler)
+    application.add_handler(remove_handler)
+    application.add_handler(toggle_handler)
     
     application.add_handler(private_msg_handler)
     application.add_handler(group_msg_handler)
